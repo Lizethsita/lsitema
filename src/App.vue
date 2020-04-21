cebb4d6b3fe0086a97e6b2e2fa105a5239961c6a
Lizethsita <lizeth_duque@hotmail.es>
2020-04-21 16:55:25 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,210 @@
+<template>
+	<v-app id="inspire">
+		<v-navigation-drawer v-model="drawer" :clipped="$vuetify.breakpoint.lgAndUp" fixed app>
+			<v-list dense>
+				<template>
+					<v-list-item :to="{ name: 'Home' }">
+						<v-list-item-action>
+							<v-icon>home</v-icon>
+						</v-list-item-action>
+						<v-list-item-title>
+							Inicio
+						</v-list-item-title>
+					</v-list-item>
+				</template>
+				<template>
+					<v-list-group>
+						<v-list-item slot="activator">
+							<v-list-item-content>
+								<v-list-item-title>
+									Almacén
+								</v-list-item-title>
+							</v-list-item-content>
+						</v-list-item>
+						<v-list-item :to="{ name: 'categorias' }">
+							<v-list-item-action>
+								<v-icon>table_chart</v-icon>
+							</v-list-item-action>
+							<v-list-item-content>
+								<v-list-item-title>
+									Categorías
+								</v-list-item-title>
+							</v-list-item-content>
+						</v-list-item>
+						<v-list-item :to="{ name: '' }">
+							<v-list-item-action>
+								<v-icon>table_chart</v-icon>
+							</v-list-item-action>
+							<v-list-item-content>
+								<v-list-item-title>
+									Artículos
+								</v-list-item-title>
+							</v-list-item-content>
+						</v-list-item>
+					</v-list-group>
+				</template>
+				<template>
+					<v-list-group>
+						<v-list-item slot="activator">
+							<v-list-item-content>
+								<v-list-item-title>
+									Compras
+								</v-list-item-title>
+							</v-list-item-content>
+						</v-list-item>
+						<v-list-item :to="{ name: '' }">
+							<v-list-item-action>
+								<v-icon>table_chart</v-icon>
+							</v-list-item-action>
+							<v-list-item-content>
+								<v-list-item-title>
+									Ingresos
+								</v-list-item-title>
+							</v-list-item-content>
+						</v-list-item>
+						<v-list-item :to="{ name: '' }">
+							<v-list-item-action>
+								<v-icon>table_chart</v-icon>
+							</v-list-item-action>
+							<v-list-item-content>
+								<v-list-item-title>
+									Proveedores
+								</v-list-item-title>
+							</v-list-item-content>
+						</v-list-item>
+					</v-list-group>
+				</template>
+				<template>
+					<v-list-group>
+						<v-list-item slot="activator">
+							<v-list-item-content>
+								<v-list-item-title>
+									Ventas
+								</v-list-item-title>
+							</v-list-item-content>
+						</v-list-item>
+						<v-list-item :to="{ name: '' }">
+							<v-list-item-action>
+								<v-icon>table_chart</v-icon>
+							</v-list-item-action>
+							<v-list-item-content>
+								<v-list-item-title>
+									Ventas
+								</v-list-item-title>
+							</v-list-item-content>
+						</v-list-item>
+						<v-list-item :to="{ name: '' }">
+							<v-list-item-action>
+								<v-icon>table_chart</v-icon>
+							</v-list-item-action>
+							<v-list-item-content>
+								<v-list-item-title>
+									Clientes
+								</v-list-item-title>
+							</v-list-item-content>
+						</v-list-item>
+					</v-list-group>
+				</template>
+				<template>
+					<v-list-group>
+						<v-list-item slot="activator">
+							<v-list-item-content>
+								<v-list-item-title>
+									Accesos
+								</v-list-item-title>
+							</v-list-item-content>
+						</v-list-item>
+						<v-list-item :to="{ name: '' }">
+							<v-list-item-action>
+								<v-icon>table_chart</v-icon>
+							</v-list-item-action>
+							<v-list-item-content>
+								<v-list-item-title>
+									Roles
+								</v-list-item-title>
+							</v-list-item-content>
+						</v-list-item>
+						<v-list-item :to="{ name: '' }">
+							<v-list-item-action>
+								<v-icon>table_chart</v-icon>
+							</v-list-item-action>
+							<v-list-item-content>
+								<v-list-item-title>
+									Usuarios
+								</v-list-item-title>
+							</v-list-item-content>
+						</v-list-item>
+					</v-list-group>
+				</template>
+				<template>
+					<v-list-group>
+						<v-list-item slot="activator">
+							<v-list-item-content>
+								<v-list-item-title>
+									Consultas
+								</v-list-item-title>
+							</v-list-item-content>
+						</v-list-item>
+						<v-list-item :to="{ name: '' }">
+							<v-list-item-action>
+								<v-icon>table_chart</v-icon>
+							</v-list-item-action>
+							<v-list-item-content>
+								<v-list-item-title>
+									Consultas Compras
+								</v-list-item-title>
+							</v-list-item-content>
+						</v-list-item>
+						<v-list-item :to="{ name: '' }">
+							<v-list-item-action>
+								<v-icon>table_chart</v-icon>
+							</v-list-item-action>
+							<v-list-item-content>
+								<v-list-item-title>
+									Consultas Ventas
+								</v-list-item-title>
+							</v-list-item-content>
+						</v-list-item>
+					</v-list-group>
+				</template>
+			</v-list>
+		</v-navigation-drawer>
+
+		<v-app-bar :clipped-left="$vuetify.breakpoint.lgAndUp" app color="blue darken-3" dark>
+			<v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+			<v-toolbar-title style="width: 300px" class="ml-0 pl-4">
+				<span class="hidden-sm-and-down">Sistema</span>
+			</v-toolbar-title>
+			<v-spacer />
+			<v-btn icon>
+				<v-icon>apps</v-icon>
+			</v-btn>
+		</v-app-bar>
+		<v-content>
+			<v-container class="fill-height" fluid>
+				<v-slide-y-transition mode="out-in">
+					<router-view />
+				</v-slide-y-transition>
+			</v-container>
+		</v-content>
+
+		<template>
+			<v-footer padless color="primary" class="white--text">
+				<v-col class="text-center" cols="12">
+					{{ new Date().getFullYear() }} — <strong>Lizethsita</strong>
+				</v-col>
+			</v-footer>
+		</template>
+	</v-app>
+</template>
+
+<script>
+export default {
+	name: 'App',
+	data() {
+		return {
+			drawer: null
+		};
+	}
+};
+</script>
